04afd675007c1edff13dfd486af0ba83a1bd12ff
Andy Sterkowitz <7583162+andysterks@users.noreply.github.com>
2024-09-17 01:57:46 +0000

    Sep 16, 2024, 9:57 PM

--- src/Node.js
+++ src/Node.js
@@ -1,7 +1,8 @@
 export default class Node {
-  constructor(value, left = null, right = null) {
+  constructor(value, left = null, right = null, parent = null) {
     this.value = value;
     this.left = left;
     this.right = right;
+    this.parent = parent;
   }
 }
