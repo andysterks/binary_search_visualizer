e9463f2f38e13b564083df5544a459fa41be3d20
Andy Sterkowitz <7583162+andysterks@users.noreply.github.com>
2024-09-17 02:05:48 +0000

    Sep 16, 2024, 10:05 PM

--- src/functions/Insert.js
+++ src/functions/Insert.js
@@ -11,11 +11,12 @@ export default function Insert(newNode, existingNode) {
     if (!existingNode.right) {
       return new Node(existingNode.value, existingNode.left, new Node(newNode.value, null, null, existingNode), existingNode.parent);
     } else {
-      return new Node(existingNode.value, existingNode.left, Insert(newNode, existingNode.right));
+      //return new Node(existingNode.value, existingNode.left, Insert(newNode, existingNode.right));
+      return InsertDuplicateRight(existingNode, Insert(newNode, existingNode.right));
     }
   }
 }
 
 function InsertDuplicateRight(existingNode, newNode) {
-  return new Node(existingNode.value, existingNode.left, new Node(newNode.value, null, null, existingNode), existingNode.parent);
+  return new Node(existingNode.value, existingNode.left, newNode);
 }
