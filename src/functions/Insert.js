da44923ac290e7c4ff72bd0cf4ae9d019c4bc268
Andy Sterkowitz <7583162+andysterks@users.noreply.github.com>
2024-09-17 02:00:55 +0000

    Sep 16, 2024, 10:00 PM

--- src/functions/Insert.js
+++ src/functions/Insert.js
@@ -3,13 +3,13 @@ import Node from "../Node"
 export default function Insert(newNode, existingNode) {
   if (newNode.value < existingNode.value) {
     if (!existingNode.left) {
-      return new Node(existingNode.value, new Node(newNode.value), existingNode.right);
+      return new Node(existingNode.value, new Node(newNode.value, null, null, existingNode), existingNode.right, existingNode.parent);
     } else {
       return new Node(existingNode.value, Insert(newNode, existingNode.left), existingNode.right);
     }
   } else {
     if (!existingNode.right) {
-      return new Node(existingNode.value, existingNode.left, new Node(newNode.value));
+      return new Node(existingNode.value, existingNode.left, new Node(newNode.value, null, null, existingNode), existingNode.parent);
     } else {
       return new Node(existingNode.value, existingNode.left, Insert(newNode, existingNode.right));
     }
