2cc55e4da9730c3f9583014f748f4c3ab3860b49
Andy Sterkowitz <7583162+andysterks@users.noreply.github.com>
2024-09-22 23:33:01 +0000

    Sep 22, 2024, 7:33 PM

--- src/functions/IsUnary.js
+++ src/functions/IsUnary.js
@@ -1,3 +1,3 @@
 export function IsUnary(node) {
-  return node.left !== null && node.right === null || node.left === null && node.right !== null;
+  return (node.left !== null && node.right === null) || (node.left === null && node.right !== null);
 }
