ca129f39cf3d3ab9d901ab3bf3bebe1e552b108c
Andy Sterkowitz <7583162+andysterks@users.noreply.github.com>
2024-09-22 23:54:09 +0000

    Sep 22, 2024, 7:54 PM

--- src/functions/Insert.js
+++ src/functions/Insert.js
@@ -5,63 +5,27 @@ import { IsLeaf } from "./IsLeaf"
 import { IsUnary } from "./IsUnary"
 
 export default function Insert(newNode, existingNode) {
+  if (!existingNode) {
+    return newNode;
+  }
+
   if (newNode.value < existingNode.value) {
-    if (IsRoot(existingNode)) {
-      if (existingNode.left?.value === newNode.value || existingNode.right?.value === newNode.value) { 
-        console.log("duplicate");
-        return existingNode;
-      }
-      
-      if (existingNode.left === null) {
-        return AddLeft(existingNode, newNode);
-      } else {
-        return Insert(newNode, existingNode.left);
-      }
-    } else {
-      debugger;
-    }
+    return new Node(
+      existingNode.value,
+      Insert(newNode, existingNode.left),
+      existingNode.right,
+      existingNode.parent
+    );
+  } else if (newNode.value > existingNode.value) {
+    return new Node(
+      existingNode.value,
+      existingNode.left,
+      Insert(newNode, existingNode.right),
+      existingNode.parent
+    );
   } else {
-    if (IsRoot(existingNode)) {
-      if (existingNode.left?.value === newNode.value || existingNode.right?.value === newNode.value) { 
-        console.log("duplicate");
-        return existingNode;
-      }
-
-      if (existingNode.right == null) {
-        return AddRight(existingNode, newNode);
-      } else {
-        return Insert(newNode, existingNode.right);
-      }
-    }
-    if (existingNode.right === null) {
-      if (existingNode.left?.value === newNode.value || existingNode.right?.value === newNode.value) { 
-        console.log("duplicate");
-        return existingNode;
-      }
-
-      if (IsLeaf(existingNode) && IsUnary(existingNode.parent)) {
-        if (newNode.value > existingNode.value) {
-          // shove up existing node (5), set original parent to left (3), set new (7) to right
-          var newParent = new Node(
-            existingNode.value,
-            null,
-            null,
-            existingNode.parent.parent
-          );
-
-          AddLeft(newParent, existingNode.parent);
-
-          return newParent;
-        } else {
-          // shove up existing node, set parent to right node
-        }
-      }
-
-      return AddRight(existingNode, newNode);
-    } else {
-      //return new Node(existingNode.value, existingNode.left, Insert(newNode, existingNode.right));
-      return InsertDuplicateRight(existingNode, Insert(newNode, existingNode.right));
-    }
+    // If the value is equal, you can choose to ignore it or handle duplicates
+    return existingNode;
   }
 }
 
