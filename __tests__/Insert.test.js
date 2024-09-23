45b83f56db4628ad2a47be46b96a5c00dd7b477b
Andy Sterkowitz <7583162+andysterks@users.noreply.github.com>
2024-09-23 01:44:07 +0000

    Sep 22, 2024, 9:44 PM

--- __tests__/Insert.test.js
+++ __tests__/Insert.test.js
@@ -1,7 +1,7 @@
 import Insert from '../src/functions/Insert.js';
 import Node from '../src/Node.js';
 
-describe('Insert function', () => {
+describe('Simple Insert functions', () => {
   test('should insert a new node into an empty tree', () => {
     const result = Insert(5, null);
     expect(result.value).toBe(5);
@@ -31,6 +31,26 @@ describe('Insert function', () => {
     root = Insert(7, root);
     root = Insert(1, root);
     root = Insert(9, root);
+
+    expect(root.value).toBe(5);
+    expect(root.left.value).toBe(3);
+    expect(root.right.value).toBe(7);
+    expect(root.left.left.value).toBe(1);
+    expect(root.right.right.value).toBe(9);
+  });
+
+  test('should handle inserting multiple values', () => {
+    let root = new Node(2);
+    expect(root.value).toBe(2);
+    root = Insert(3, root);
+    expect(root.value).toBe(2);
+    expect(root.right.value).toBe(3);
+    expect(root.left).toBeNull();
+    root = Insert(1, root);
+    expect(root.value).toBe(2);
+    expect(root.left.value).toBe(1);
+    root = Insert(4, root);
+    root = Insert(5, root);
 
     expect(root.value).toBe(5);
     expect(root.left.value).toBe(3);
